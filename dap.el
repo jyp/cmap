9dbcdea5b710429a0d700831da320d741311550d
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2021-01-27 20:02:01 +0100

    doc

--- dap.el
+++ dap.el
@@ -3,10 +3,31 @@
 ;; Copyright (C) 2021  Jean-Philippe Bernardy
 ;; Copyright (C) 2020  Omar Antolín Camarena
 
+;; Author: Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
+;; Version: 0
+;; Keywords: extensions, convenience
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 ;;; Commentary:
 
 ;; Act on relevant object at point.  The main entry point is `dap-dap'.
 ;; The main configuration entry point is `dap-targets'.
+
+
+;; Originally a fork of `embark', this is an almost complete rewrite focused on actions in regular buffers, instead of the minibuffer. 
 
 ;;; Code:
 
