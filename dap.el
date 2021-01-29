401e62f6854a2fb81c327b4f6c948bf078d83242
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2021-01-29 15:24:19 +0100

    tweak

--- dap.el
+++ dap.el
@@ -168,10 +168,12 @@ BINDINGS is the list of bindings."
 
 (defun dap-symbol-next (sym)
   (interactive)
+  (forward-char)
   (re-search-forward (regexp-quote sym)))
 
 (defun dap-symbol-prev (sym)
   (interactive)
+  (backward-char)
   (re-search-backward (regexp-quote sym)))
 
 (dap-define-keymap dap-symbol-map
